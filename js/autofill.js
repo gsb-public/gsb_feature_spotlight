fcc6fec5dd8c72270809059d231b16dafc060acb
Adam Moore <redndahead@gmail.com>
2013-04-06 14:38:05 -0700

    Update for new fields.

--- js/autofill.js
+++ js/autofill.js
@@ -2,18 +2,18 @@
   Drupal.behaviors.gsbFeatureSpotlight = {
     attach: function (context) {
       // Hide the nid field.
-      $('.field-name-field-gsb-spotlight-profile-nid, .field-name-field-gsb-spotlight-profile-fid' ).hide();
+      $('.field-name-field-spotlight-person-nid, .field-name-field-spotlight-person-fid' ).hide();
 
       // Set our autocomplete on load.
       Drupal.gsbFeatureSpotlight.toggleAutocomplete();
 
-      oldFid = $(':input[name="field_gsb_spotlight_profile_fid[und][0][value]"]').val();
+      oldFid = $(':input[name="field_spotlight_person_fid[und][0][value]"]').val();
       if (Math.floor(oldFid) == oldFid && $.isNumeric(oldFid)) {
         $('#gsb-person-image-wrapper').load(Drupal.settings.basePath + 'gsb_feature_spotlight_create_thumbnail/' + oldFid);
       }
 
       // If they choose a type adjust the autocomplete.
-      $('input[name="field_gsb_spotlight_person_type[und]"]').click(function() {
+      $('input[name="field_spotlight_person_type[und]"]').click(function() {
         Drupal.gsbFeatureSpotlight.toggleAutocomplete();
       });
     }
@@ -22,7 +22,7 @@
   Drupal.gsbFeatureSpotlight = Drupal.gsbFeatureSpotlight || {}
   Drupal.gsbFeatureSpotlight.toggleAutocomplete = function() {
     // Get the chosen type
-    personType = $('input[name="field_gsb_spotlight_person_type[und]"]:checked').val();
+    personType = $('input[name="field_spotlight_person_type[und]"]:checked').val();
 
     // Add class to the body element to hide the autocomplete.
     // @TODO Add the allowed types via javascript settings so we can have any number of types.
@@ -43,14 +43,14 @@
           title = data[3];
           link = data[4];
           fid = data[5];
-          $(':input[name="field_gsb_spotlight_profile_nid[und][0][value]"]').val(nid);
+          $(':input[name="field_spotlight_person_nid[und][0][value]"]').val(nid);
           $(':input[name="field_first_name[und][0][value]"]').val(firstName);
           $(':input[name="field_last_name[und][0][value]"]').val(lastName);
-          $(':input[name="field_title_position[und][0][value]"]').val(title);
-          $(':input[name="field_gsb_spotlight_profile_fid[und][0][value]"]').val(fid);
+          $(':input[name="field_title_position_single[und][0][value]"]').val(title);
+          $(':input[name="field_spotlight_person_fid[und][0][value]"]').val(fid);
 
           $('#gsb-person-image-wrapper').load(Drupal.settings.basePath + 'gsb_feature_spotlight_create_thumbnail/' + fid);
-          $(':input[name="field_gsb_spotlight_link[und][0][url]"]').val(link);
+          $(':input[name="field_link_single[und][0][url]"]').val(link);
         }
       });
     }
